df022b0ef4b762ccc26faab32d213a2199cab840
vsonani <152912926+vsonani888@users.noreply.github.com>
2025-05-09 01:47:11 -0400

    Final done finish im tired

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # Cont-and-Kukanov-Back-testing
+
+Simulation of a smart order routing strategy using the static cost model from Cont and Kukanov's "Optimal Order Placement in Limit Order Markets". Goal is to split 5000 shares across multiple venues to reduce the total cost.
+
+## Overview of backtest.py
+
+- Extracts the l1_day.csv to get the prices and sizes for the venue over a period of time
+- Implements a grid function that parses over all of the permuations of ranges for the lambda over, under and the theta queue values with steps. 
+- During the permutations, it uses the backtest to find the best cost which later updates all of the best values that will be used for the results
+- Backtest uses allocate function to find the split value of how much share it needs to buy at the certain venue.
+- The results from the optimal permutations are compared with the twap, vwap and the bast ask values. 
+- Best ask buys from the venue with the lowest ask price, Twap spilts it between equal buckets, and Vwap splits it depending on the available size per venue.
+
+
+## Output
+
+- The best parameter for the lambda and theta are found and outputed for the backtest which can be used later for different tests and implementation.
+- The total cost and the avg price of the test data given.
+- comparision with the benchmark twap, vwap and the best ask.
+- Generates a results.png file which shows how the cost change over time
+
+## Running the file
+
+```bash
+python backtest.py
--- backtest.py
+++ backtest.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import numpy as np
+import json
+import matplotlib.pyplot as plt
 
 def read_file(file_path):
     df = pd.read_csv(file_path)
@@ -86,43 +88,245 @@ def compute_cost(alloc, venues, order_size, lambda_over, lambda_under, theta_que
 
     return cash_spent + risk_pen + file_pen
 
+def grid_search(data, lo_range, lu_range, tq_range, lo_step, lu_step, tq_step, order_size):
+    best_lo = None
+    best_lu = None
+    best_tq = None
+
+    best_cost = float('inf')
+    best_avg_price = 0
+    best_curve = []
+
+    #make array of all values by adding the step size
+
+    lo_values = np.arange(lo_range[0], lo_range[1] + lo_step, lo_step)
+    lu_values = np.arange(lu_range[0], lu_range[1] + lu_step, lu_step)
+    tq_values = np.arange(tq_range[0], tq_range[1] + tq_step, tq_step)
+
+    #runs it for each of the values 
+
+    for lo in lo_values:
+        for lu in lu_values:
+            for tq in tq_values:
+                cost, avg_price, curve = backtest(data, lo, lu, tq, order_size)
+
+                if cost < best_cost:
+                    best_cost = cost
+                    best_avg_price = avg_price
+                    best_lo = lo
+                    best_lu = lu
+                    best_tq = tq
+                    best_curve = curve
+
+    return best_lo, best_lu, best_tq, best_cost, best_avg_price, best_curve
+
+
+def backtest(data, lo, lu, tq, order_size):
+
+    remaining_size = order_size
+    total_cost = 0.0
+    total_shares = 0
+    cost_over_time = []
+
+    filled = False
+
+    for ts, venues in data:
+
+        if not filled:
+
+            #gets the split value that we can use to find the value
+
+            split = allocate(remaining_size, venues, lo, lu, tq)
+
+            if split:
+
+                for i in range(len(venues)):
+
+                    qty = split[i]
+                    ask = venues[i]['ask']
+                    fee = venues[i]['fee']
+                    rebate = venues[i]['rebate']
+                    ask_size = venues[i]['ask_size']
+
+                    #depending on the split we buy the shares while saving the costs and such
+                    
+                    exe = min(qty, ask_size)
+                    cost = exe * (ask + fee)
+                    rebate_amt = max(qty - exe, 0) * rebate
+
+                    total_cost += cost - rebate_amt
+                    total_shares += exe
+                    remaining_size -= exe
+                
+                if remaining_size <= 0:
+                    filled = True
+
+        cost_over_time.append(total_cost)
+
+    avg_price = total_cost / total_shares
+
+    return total_cost, avg_price, cost_over_time
+
+def baseline_best_ask(data, order_size):
+    remaining_size = order_size
+    total_cost = 0.0
+    total_shares = 0
+
+    for ts, venues in data:
+
+        #basically buys the shares starting from the cheapest venue
+
+        best = venues[0]
+
+        for venue in venues:
+            if venue['ask'] < best['ask']:
+                best = venue
+
+        qty = min(remaining_size, best['ask_size'])
+
+        cost = qty * (best['ask'] + best['fee'])
+        total_cost += cost
+        total_shares += qty
+        remaining_size -= qty
+
+    avg_price = total_cost/total_shares
+
+    return total_cost, avg_price
+
+def baseline_twap(data, order_size):
+    remaining_size = order_size
+    total_cost = 0.0
+    total_shares = 0
+
+    #bascially divide up the order size into 9 blocks that would buy the shares
+
+    block = 9
+    block_size = order_size // block
+    interval = len(data) // block
+
+    for i in range(block):
+        
+        idx = i * interval
+        venues = data[idx][1]
+        share_left = block_size
+
+        for v in venues:
+            qty = min(share_left, v['ask_size'], remaining_size)
+            cost = qty * (v['ask'] + v['fee'])
+
+            total_cost += cost
+            total_shares += qty
+            remaining_size -= qty
+            share_left -= qty
+
+        #if it cannot be filled than we just force feed it
+
+        j = idx + 1
+
+        while share_left > 0 and j < len(data):
+            for venue in data[j][1]:
+
+                qty = min(share_left, v['ask_size'], remaining_size)
+                cost = qty * (v['ask'] + v['fee'])
+
+                total_cost += cost
+                total_shares += qty
+                remaining_size -= qty
+                share_left -= qty
+            
+            j += 1
+
+
+    avg_price = total_cost/total_shares
+
+    #print("toal shares",  total_shares)
+
+    return total_cost, avg_price
+
+
+def baseline_vwap(data, order_size):
+    remaining_size = order_size
+    total_cost = 0.0
+    total_shares = 0
+
+    for ts, venues in data:
+
+        #bascially buys shares depending on the number of shares each venue gives, splits it evenly
+
+        total_vol = 0
+
+        for v in venues:
+            total_vol += v['ask_size']
+
+        for venue in venues:
+
+            share = venue['ask_size'] / total_vol
+            qty = min(share * remaining_size, venue['ask_size'])
+
+            cost = qty * (venue['ask'] + venue['fee'])
+            total_cost += cost
+            total_shares += qty
+            remaining_size -= qty
+
+    avg_price = total_cost / total_shares 
+
+    return total_cost, avg_price
+    
 if __name__ == "__main__":
     file_path = 'l1_day.csv'
     order_size =  5000
 
-    total_cost = 0.0
-    total_shares = 0
-    lambda_over = 0.01
-    lambda_under = 0.02
-    theta_queue = 0.001
-
     data = read_file(file_path)
 
-    remaining_size = order_size
+    lo_range = (0.001, 0.01)
+    lo_step = 0.002
+    lu_range = (0.01, 0.1)
+    lu_step = 0.02
+    tq_range = (0.0001, 0.001)
+    tq_step = 0.003
 
-    for ts, venues in data:
+    #updates the best vales depending on the range of all values applied for each backtest
 
-        split = allocate(remaining_size, venues, lambda_over, lambda_under, theta_queue)
+    best_lo, best_lu, best_tq, best_cost, best_avg_price, best_curve = grid_search(data, lo_range, lu_range, tq_range, lo_step, lu_step, tq_step, order_size)
 
-        if not split:
-            continue
+    #some tests to get some benchmarks
+    
+    best_ask_cost, best_ask_avg = baseline_best_ask(data, order_size)
+    twap_cost, twap_avg = baseline_twap(data, order_size)
+    vwap_cost, vwap_avg = baseline_vwap(data, order_size)
 
-        for i in range(len(venues)):
+    #output result in json format
+    
+    result = {
 
-            qty = split[i]
-            ask = venues[i]['ask']
-            fee = venues[i]['fee']
-            rebate = venues[i]['rebate']
-            ask_size = venues[i]['ask_size']
-            
-            exe = min(qty, ask_size)
-            cost = exe * (ask + fee)
-            rebate = max(qty - exe, 0) * rebate
+        "best_weights" : {"lambda_over" : best_lo, "lambda_under" : best_lu, "theta_queue" : best_tq},
 
-            total_cost += cost - rebate
-            total_shares += exe
-            remaining_size -= exe
+        "best_cost" : best_cost,
+        "best_avg_price" : best_avg_price,
 
-    avg_price = total_cost/ total_shares
+        "best_ask_cost" : best_ask_cost,
+        "best_ask_avg_price" : best_ask_avg,
 
-    print("avg price", avg_price)
+        "twap_cost" : twap_cost,
+        "twap_avg" : twap_avg,
+
+        "vwap_cost" : vwap_cost,
+        "vwap_avg" : vwap_avg,
+
+        "savings_vs_best_ask" : (best_ask_cost - best_cost) / best_ask_cost,
+        "savings_vs_twap" : (twap_cost - best_cost) / twap_cost,
+        "savings_vs_vwap" : (vwap_cost - best_cost) / vwap_cost
+
+    }
+
+    #print(twap_cost," ",  best_cost)
+
+    print(json.dumps(result))
+
+    print(best_curve[:10])
+    print(len(best_curve))
+
+    plt.plot(best_curve)
+    plt.savefig("results.png")
+
+    #print("avg price", avg_price)
